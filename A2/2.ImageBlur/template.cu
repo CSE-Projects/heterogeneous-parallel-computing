8d22d718a2eb6acee9805a0b382c18f4bf9f7581
Omkar Prabhu <omkkarprabuinfinity@gmail.com>
2018-08-06 08:24:01 +0530

    A2: fixes

--- A2/2.ImageBlur/template.cu
+++ A2/2.ImageBlur/template.cu
@@ -11,19 +11,21 @@
 __global__ void gauss_blur_image(float* input, float* output, int image_height, int image_width) {
 	// calculating x coordinate and the y coordinate
 	// calculated on the basis of block index and thread index
-	int X = blockIdx.x * blockDim.x + threadIdx.x;
-    int Y = blockIdx.y * blockDim.y + threadIdx.y;
+	int X = blockIdx.y * blockDim.y + threadIdx.y;
+    int Y = blockIdx.x * blockDim.x + threadIdx.x;
 
 	// compute blurred value in the blur size grid
-    float blurred_value = 0;
-    int count=0;
+    float blurred_val1=0, blurred_val2 = 0, blurred_val3 = 0;
+	int count=0;
 
 	// considering 9 values (consecutive in the input)
     for(int i =  X - BLUR_SIZE; i <= X + BLUR_SIZE; i++) {
       for(int j = Y - BLUR_SIZE; j <= Y + BLUR_SIZE; j++) {
 		// checking if the index is valid or not
-		if(i >= 0 && j>=0 && i < image_height && j < image_width) { 
-			blurred_value += input[(i*image_width + j)];
+		if(i >= 0 && j >= 0 && i < image_height && j < image_width) { 
+			blurred_val1 += input[3 * (i*image_width + j)];
+            blurred_val2 += input[3 * (i*image_width + j) + 1];
+            blurred_val3 += input[3 * (i*image_width + j) + 2];
 			// incrementing the count of valid indices
 			count++;  
         }
@@ -31,11 +33,17 @@ __global__ void gauss_blur_image(float* input, float* output, int image_height, 
 	}
 
 	// average
-    blurred_value = blurred_value / count;
+    blurred_val1 = blurred_val1/ count;
+    blurred_val2 = blurred_val2/ count;
+    blurred_val3 = blurred_val3/ count;
 	// update output
-	if (X < image_height && Y < image_width && X>-1 && Y>-1) {
-		output[(X*image_width + Y)] = blurred_value;
-	}
+	blurred_val1 = blurred_val1/ count;
+    blurred_val2 = blurred_val2/ count;
+    blurred_val3 = blurred_val3/ count;
+
+    output[ 3 * (X*image_width + Y)] = blurred_val1;
+    output[ 3 * (X*image_width + Y) + 1] = blurred_val2;
+    output[ 3 * (X*image_width + Y) + 2] = blurred_val3;
 }
 
 int main(int argc, char *argv[]) {
@@ -89,8 +97,8 @@ int main(int argc, char *argv[]) {
 	///////////////////////////////////////////////////////
 	wbTime_start(Compute, "Doing the computation on the GPU");
 	//  initialize thread block and kernel grid dimensions
-	dim3 threads(30, 30, 1);
-	dim3 blocks(imageHeight/30 + 1, imageWidth/30 + 1, 1);
+	dim3 threads(16, 16, 1);
+	dim3 blocks(imageHeight/16 + 1, imageWidth/16 + 1, 1);
   
 	// invoke CUDA kernel
 	gauss_blur_image<<<threads, blocks>>> (deviceInputImageData, deviceOutputImageData, imageHeight, imageWidth);
